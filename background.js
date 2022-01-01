8f527f965cdd4256ec2202d0e27cb9df823286ae
Yoni-Satat <yoni.satat@gmail.com>
2022-01-01 18:20:57 +0000

    add default bookmark

--- background.js
+++ background.js
@@ -1,1 +1,12 @@
-console.log('hello from background.js');
+let favourites = [{
+    title: 'My Bookmarks',
+    url: 'https://www.example.com',
+    favIconUrl: 'https://www.flaticon.com/'
+}];
+
+chrome.runtime.onInstalled.addListener(() => {
+    // access chrome storage and set default values
+    chrome.storage.sync.set({
+        favourites
+    });
+});

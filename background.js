0a56f028ebbe3880ea6268e24fbb8ce327d462c1
Yoni-Satat <yoni.satat@gmail.com>
2022-01-05 16:11:11 +0000

    fix url to favIconUrl

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 let favourites = [{
     title: 'Freepik',
     url: 'https://www.flaticon.com/',
-    favIconUrl: 'https://www.flaticon.com/'
+    favIconUrl: 'https://media.flaticon.com/dist/min/img/favicon.ico'
 }];
 
 chrome.runtime.onInstalled.addListener(() => {
